0a282c96b2103295a7c5b95797a31f283af47035
Gabriel Galc <gabriel.galc@vtex.com.br>
2018-10-18 01:58:10 -0300

    Add empty state

--- react/components/BlockedFinalists.tsx
+++ react/components/BlockedFinalists.tsx
@@ -1,7 +1,18 @@
 import React, { Component } from 'react'
+import OwlIcon from '../icons/OwlIcon'
 
 export default class FinalistsList extends Component<{}> {
   public render () {
-    return <p>Brilha, galc</p>
+    return (
+      <div className="flex justify-center w-100">
+        <div className="bg-base ma5 ba1 b-dark-silver center mw7 pa7 mt7 br2 w-100">
+          <div className="flex items-center justify-center pt9">
+            <OwlIcon height="100" />
+          </div>
+          <div className="f3 fw7 tc pt9">Nothing to see here, mate</div>
+          <div className="f5 tc pb9 pt3">Go get some pizza and come back after Formula is done.</div>
+        </div>
+      </div>
+    )
   }
 }
